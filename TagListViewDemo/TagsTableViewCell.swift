f7cb3a941dca1b0d2d94d30fffa8965b9c2e476f
Arnaud Leene <aleene@gmail.com>
2016-11-27 19:07:37 +0100

    Update TagsTableViewCell.swift

--- TagListViewDemo/TagsTableViewCell.swift
+++ TagListViewDemo/TagsTableViewCell.swift
@@ -1,5 +1,5 @@
 //
-//  TagTableViewController.swift
+//  TagsTableViewCell.swift
 //  TagListViewDemo
 //
 //  Created by arnaud on 23/11/16.
@@ -7,123 +7,37 @@
 //
 
 import UIKit
+import TagListView
 
-class TagTableViewController: UITableViewController, TagListViewDelegate, TagListViewDatasource {
-
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
+class TagsTableViewCell: UITableViewCell {
     
-    private var tags = ["This", "is", "a", "set", "of", "tags", "in", "a", "tableViewCell", ".", "There", "are", "a", "lot", "of", "tags", "defined", "here", "in", "order", "to", "show", "the", "dynamic", "cell", "height", "LAST ONE"]
-    
-    fileprivate struct Storyboard {
-        static let TagListViewCellIdentifier = "TagListView Cell"
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: Storyboard.TagListViewCellIdentifier, for: indexPath) as! TagsTableViewCell
-        cell.tagList = tags
-        cell.tagListView?.delegate = self
-        cell.tagListView?.datasource = self
-        cell.editMode = editMode
-        return cell
-    }
-    
-    // MARK: - TagListView Delegates
-    
-    func tagListView(_ tagListView: TagListView, didSelectTagAtIndex index: Int) {
-        print("The tag with index", index, "has been selected")
-        print(tagListView.selectedTags().count, "tags have been selected")
-        if editMode {
-            tagListView.removeTag(at: index)
-            tags.remove(at: index)
-        }
-    }
-    
-    func tagListView(_ tagListView: TagListView, willSelectTagAtIndex index: Int) -> Int {
-        print("The tag with index ", index, "will be selected")
-        return index
-        // return a negative number is a tag at index may not be selected
-    }
-    
-    func tagListView(_ tagListView: TagListView, didDeselectTagAtIndex index: Int) {
-        print("The tag with index ", index, "has been DEselected")
-    }
-    
-    func tagListView(_ tagListView: TagListView, willDeselectTagAtIndex index: Int) -> Int {
-        print("The tag with index ", index, "will be DEselected")
-        return index
-        // return a negative number is a tag at index may not be selected
-    }
-
-    func tagListView(_ tagListView: TagListView, canEditTagAtIndex index: Int) -> Bool {
-        print("The tag with ", index, "can be edited")
-        return editMode
-    }
-
-    func tagListView(_ tagListView: TagListView, willBeginEditingTagAt index: Int) {
-        print("The tag with ", index, "will be edited")
-        // you can delete the corresponding data here
-    }
-
-    func tagListView(_ tagListView: TagListView, didEndEditingTagAt index: Int) {
-        print("The tag with ", index, "has been edited")
-    }
-
-    // MARK: editMode functions and outley/actions
-    
-    private var editMode = false {
+    var tagList: [String] = [] {
         didSet {
-            if editMode != oldValue {
-                setEditModeBarButtonItemTitle()
-            }
+            setupTagListViewWithData()
         }
     }
     
-    private func setEditModeBarButtonItemTitle() {
-        if editModeBarButtonItem != nil {
-            editModeBarButtonItem.title = editMode ? "Select" : "Edit"
+    private func setupTagListViewWithData() {
+        if tagListView != nil {
+            tagListView.removeAllTags()
+            tagListView.addTags(tagList)
+            tagListView.isEditable = editMode
         }
     }
 
-    @IBOutlet weak var editModeBarButtonItem: UIBarButtonItem! {
+    var editMode = false {
         didSet {
-            setEditModeBarButtonItemTitle()
+            setupTagListViewWithData()
         }
     }
     
-    // This button allows to toggle between editMode and non-editMode
-
-    @IBAction func editBarButtonTapped(_ sender: UIBarButtonItem) {
-        editMode = !editMode
-        tableView.reloadData()
+    @IBOutlet weak var tagListView: TagListView! {
+        didSet {
+            tagListView.textFont = UIFont.preferredFont(forTextStyle: UIFontTextStyle.body)
+            tagListView.alignment = .center
+            tagListView.tagBackgroundColor = UIColor.green
+            tagListView.cornerRadius = 10
+            setupTagListViewWithData()
+        }
     }
-
-    // MARK: - View Controller Lifecycle
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // See http://stackoverflow.com/questions/18746929/using-auto-layout-in-uitableview-for-dynamic-cell-layouts-variable-row-heights#18746930
-        
-        tableView.estimatedRowHeight = 44.0
-        tableView.rowHeight = UITableViewAutomaticDimension
-        
-        tableView.reloadData()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        // This repaints the view, so the cells get the right height.
-        // This is not always needed, try it out.
-        tableView.layoutIfNeeded()
-    }
-
 }
